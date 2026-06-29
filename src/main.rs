e73ba27bb035517028b163cc936419e3415df789
Nouman Rahman <noumanrahman@Noumans-MacBook-Air.local>
2026-06-29 20:22:27 +0530

    some awesome changes

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let mut tries: i32 = 0;
+    let number: i32 = rand::random_range(1..100);
+
+    println!("{number}");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess_num: i32 = guess
+            .trim()
+            .parse::<i32>()
+            .unwrap_or_else(|_num| 0);
+
+        tries += 1;
+
+        match guess_num.cmp(&number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You WON in {} tries!", tries);
+                break;
+            }
+        }
+    }
+}
